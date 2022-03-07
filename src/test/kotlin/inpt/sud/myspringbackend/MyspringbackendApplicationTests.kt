11ba95af15e7923373d2272b215732ae3bf05a1c
Lenovo <ibtihalhasnialaoui@gmail.com>
2022-03-07 23:14:25 +0100

    My commit

---
+++ src/test/kotlin/inpt/sud/myspringbackend/MyspringbackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package inpt.sud.myspringbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyspringbackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
